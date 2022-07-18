156df45658d1a8a300134a52f1355ac99a741323
Prateek <prateekvasanthpai@gmail.com>
2022-07-18 17:24:08 +0530

    Load the Inadian States Codes Information from a CSV File

--- IndianStateCensusAnalyserProblem/Program.cs
+++ IndianStateCensusAnalyserProblem/Program.cs
@@ -6,12 +6,16 @@ namespace IndianStateCensusAnalyserProblem
     {
         // CSV Firl Paths 
         static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
-        
+
+        static string indianStateCodeHeaders = "SrNo,State Name,TIN,StateCode";
+
         static string indianStateCensusFilePath = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\IndiaStateCensusData.csv";
         static string wrongIndianStateCensusFilePath = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\IndiaData.csv";
         static string wrongIndianStateCensusFileType = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\IndianStateCensus.txt";
         static string delimiterIndianCensusFilePath = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\IndianStateCesusDelimeter.csv";
         static string wrongHeaderIndianCensusFilePath = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\WrongIndiaStateCensusData.csv";
+
+        static string indianStateCodeFilePath = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\IndiaStateCodes.csv";
 
 
         static void Main(string[] args)
@@ -33,6 +37,9 @@ namespace IndianStateCensusAnalyserProblem
             indianCensusAdapter.LoadCensusData(delimiterIndianCensusFilePath, indianStateCensusHeaders);
             //1.4
             indianCensusAdapter.LoadCensusData(wrongHeaderIndianCensusFilePath, indianStateCensusHeaders);
+
+            // Loading the State census Data File
+            indianCensusAdapter.LoadCensusData(indianStateCodeFilePath, indianStateCodeHeaders);
         }
     }
 }
