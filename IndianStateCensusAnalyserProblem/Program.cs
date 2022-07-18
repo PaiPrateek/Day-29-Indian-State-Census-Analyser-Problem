0218ba4305f0541087dba86980083481787800a8
Prateek <prateekvasanthpai@gmail.com>
2022-07-18 11:43:59 +0530

    Welcome to Indian State Census Analyser Problem

---
+++ IndianStateCensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Problem ");
+        }
+    }
+}
